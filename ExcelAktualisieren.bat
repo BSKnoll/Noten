df3b2571d0f790b575d4635689b2f31656c28a17
Markus Knaxner <m.knoll@bs-linz3.ac.at>
2025-06-10 10:26:21 +0200

    Automatisches Update vom 2025-06-10_10-26

--- ExcelAktualisieren.bat
+++ ExcelAktualisieren.bat
@@ -8,9 +8,9 @@ REM Sicherstellen, dass Ordner existiert
 mkdir "triggerfiles" 2>nul
 @echo Erstelle FORCE TRIGGER
 REM Trigger-Datei sicher schreiben
-break > "triggerfiles\trigger.txt"
-@echo Priat god baba
-REM Warte 20 Sekunden vor dem Beenden
-timeout /t 20 > nul
+break > "triggerfiles\FORCE.txt"
+@echo Pfiat god - baba
+REM Warte 5 Sekunden vor dem Beenden
+timeout /t 5 > nul
 
 exit
